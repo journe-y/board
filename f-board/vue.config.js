384b613fcea257b626bdad946c6960b8e754ef7d
DESKTOP-S1OMUAN\jiwon <jiwon_3261@naver.com>
2022-03-03 13:26:19 +0900

    proxy제거

--- f-board/vue.config.js
+++ f-board/vue.config.js
@@ -2,11 +2,5 @@ module.exports = {
     publicPath: '/board/',
     devServer: {
         historyApiFallback: true,
-        proxy: {
-            '^/': {
-                target: 'https://toyboard.herokuapp.com',
-                changeOrigin: true
-            }
-        },
     }
 }
