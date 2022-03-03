145651c2f19e73968c7b4ca5c7e31c55537282d9
DESKTOP-S1OMUAN\jiwon <jiwon_3261@naver.com>
2022-03-03 13:27:48 +0900

    proxy제거후 build

--- b-board/app.js
+++ b-board/app.js
@@ -25,7 +25,7 @@ app.set('view engine', 'vue');
 
 sequelize.sync({ force: false })
     .then(() => {
-        console.log('데이터베이스 연결 성공');
+        console.log('데이터베이스 연결 성공-!!');
     })
     .catch((err) => {
         console.error(err);
